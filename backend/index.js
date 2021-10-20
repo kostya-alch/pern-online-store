fc605982f02732e6df2cb34a6b5fbd0a33438553
kostya-alch <kostafrompiter@gmail.com>
2021-10-20 17:08:29 +0300

    sync project and DB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
 require('dotenv').config(); // библиотечка для считывания .env файлов
 const express = require('express');
+const sequelize = require('./db');
 
 const PORT = process.env.PORT || 5000; // инициализация порта
 
 const app = express();
 
-app.listen(PORT, () => console.log(`server started working on port ${PORT}!`)); //  старт сервера
+const start = async () => {
+  try {
+    await sequelize.authenticate(); // устанавливаем соединение с БД
+    await sequelize.sync(); // сверяет состояние БД с схемой данных.
+    app.listen(PORT, () =>
+      console.log(`server started working on port ${PORT}!`)
+    ); //  старт сервера
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();
