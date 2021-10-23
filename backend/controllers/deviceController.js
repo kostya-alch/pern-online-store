a717d08a2b92627554e45cfe890e6a29f889bd8e
kostya-alch <kostafrompiter@gmail.com>
2021-10-23 14:31:15 +0300

    add getAll/getOne methods for deviceController

--- backend/controllers/deviceController.js
+++ backend/controllers/deviceController.js
@@ -1,15 +1,26 @@
-<<<<<<< HEAD
 const uuid = require('uuid');
 const path = require('path');
-const { Device } = require('../models/models');
+const { Device, Device_info } = require('../models/models');
 const apiError = require('../Error/apiError');
 class DeviceController {
   async create(req, res, next) {
     try {
-      const { name, price, brandId, typeId, info } = req.body;
-      const { img } = req.files
-      let fileName = uuid.v4() + '.jpg';
-      img.mv(path.resolve(__dirname, '..', 'static', fileName));
+      let { name, price, brandId, typeId, info } = req.body; // вытаскаем поля из БД
+      const { img } = req.files; // работа с сохранением изображения товара
+      let fileName = uuid.v4() + '.jpg'; // шаблон для jpg файлов
+      img.mv(path.resolve(__dirname, '..', 'static', fileName)); // сохранение в папку static
+
+      if (info) {
+        // создание информации о девайсе
+        info = JSON.parse(info); // т.к ответ приходит в json, нужно его распарсить
+        info.forEach((i) =>
+          Device_info.create({
+            title: i.title,
+            description: i.description,
+            deviceId: device.id,
+          })
+        );
+      }
 
       const device = await Device.create({
         name,
@@ -20,18 +31,45 @@ class DeviceController {
       });
       return res.json(device);
     } catch (error) {
-      next(apiError.badRequest(error.message));
+      next(apiError.badRequest(error.message)); // обработка ошибки
     }
-=======
-
-class DeviceController {
-  async create(req, res) {
-     
->>>>>>> 30cf9c040c9f0cd672241d52b66167039184da9e
   }
 
-  async getAll(req, res) {}
-  async getOne(req, res) {}
+  async getAll(req, res) {
+    let { brandId, typeId, limit, page } = req.body;
+    page = page || 1;
+    limit = limit || 10;
+    let offset = page * limit - limit; // формула постраничного вывода. простая. лучше не придумал
+    let devices;
+
+    if (!brandId && !typeId) {
+      devices = await Device.findAndCountAll({ limit, offset }); // логика поиска товара в бд. возможно, дублирование кода
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId, limit, offset },
+      });
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { typeId, limit, offset },
+      });
+    }
+    if (brandId && typeId) {
+      devices = await Device.findAndCountAll({
+        where: { brandId, typeId, limit, offset },
+      });
+    }
+    return res.json(devices);
+  }
+  async getOne(req, res) {
+    const { id } = req.params;
+    const device = await Device.findOne({
+      where: { id },
+      include: [{ model: Device_info, as: 'info' }],
+    });
+    return res.json(device);
+  }
 }
 
 module.exports = new DeviceController();
