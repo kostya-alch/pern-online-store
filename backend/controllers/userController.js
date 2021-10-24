30b7bc7d2eb3edd4b182ae0cf7283c2ed970aeb0
kostya-alch <kostafrompiter@gmail.com>
2021-10-24 15:19:55 +0300

    realize auth and register user with JWT token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,50 @@
 const ApiError = require('../Error/apiError');
+const bcrypt = require('bcrypt');
+const jwtToken = require('jsonwebtoken');
+const { User, Basket } = require('../models/models');
 
-ApiError;
+const jwtTokenGenerate = (id, email, role) => {
+  // функция генеритует jwt токен по новой через 24ч
+  return jwtToken.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: '24h',
+  });
+};
 class UserController {
-  async registration(req, res) {} // метод регистрации юзера
+  async registration(req, res, next) {
+    // метод регистрации юзера
+    const { email, password, role } = req.body;
 
-  async login(req, res) {} // метод авторизации юзера
+    if (!email || !password) {
+      return next(ApiError.badRequest('Неккоретный email или пароль')); // проверка на пустые значения
+    }
+    const candidate = await User.findOne({ where: { email } });
+
+    if (candidate) {
+      return next(
+        ApiError.badRequest('Пользователь с таким email уже существует')
+      );
+    }
+    const hashPassword = await bcrypt.hash(password, 5); //хешируем пароль 5 раз, много не надо
+    const user = await User.create({ email, role, password: hashPassword });
+    const basket = await Basket.create({ userId: user.id });
+    const token = jwtTokenGenerate(user.id, user.email, user.role);
+    return res.json({ token });
+  }
+
+  async login(req, res, next) {
+    // метод авторизации юзера
+    const { email, password } = req.body;
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return next(ApiError.internal('Пользователь с таким именем не найден'));
+    }
+    let comparePassword = bcrypt.compareSync(password, user.password);
+    if (!comparePassword) {
+      return next(ApiError.internal('Указан неверный пароль'));
+    }
+    const token = jwtTokenGenerate(user.id, user.email, user.role);
+    return res.json({ token });
+  }
 
   async check(req, res, next) {
     // метод на проверку авторизации
