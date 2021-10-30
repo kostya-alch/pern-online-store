0da3d8808b54addfb7c52f8867c2c0aeef9574fc
kostya-alch <kostafrompiter@gmail.com>
2021-10-30 16:16:37 +0300

    add admin panel(v. 0.1)

---
+++ frontend/src/components/modals/CreateDevice.jsx
@@ -0,0 +1,103 @@
+
+import React, { useContext, useState } from 'react'
+import { Col, Dropdown, Form, Modal, Row } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
+import { context } from '../../index'
+// модальное окно для создания  девайса 
+const CreateDevice = ({ show, onHide }) => {
+
+   const { device } = useContext(context)
+   const [info, setInfo] = useState([])
+
+   const addInfo = () => { // функция добавляет характеристику товару
+      setInfo([...info, { title: '', description: '', number: Date.now() }])
+   }
+   const removeInfo = (number) => { // функция удаляет характеристику товару
+      setInfo(info.filter(i => i.number !== number))
+   }
+
+   return (
+      <Modal
+         show={show}
+         onHide={onHide}
+         size="lg"
+         aria-labelledby="contained-modal-title-vcenter"
+         centered
+      >
+         <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+               Добавить новое устройство
+            </Modal.Title>
+         </Modal.Header>
+         <Modal.Body>
+            <Form>
+               <Dropdown className='mt-2 mb-2'>
+                  <Dropdown.Toggle>
+                     Выберите тип
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                     {device.types.map(type =>
+                        <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+                     )}
+                  </Dropdown.Menu>
+               </Dropdown>
+
+               <Dropdown className='mt-2 mb-2'>
+                  <Dropdown.Toggle>
+                     Выберите бренд
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                     {device.brands.map(brand =>
+                        <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+                     )}
+                  </Dropdown.Menu>
+               </Dropdown>
+               <Form.Control
+                  className='mt-3'
+                  placeholder='Введите стоимость устройства'
+                  type='number' />
+               <Form.Control
+                  className='mt-3'
+                  type='file' />
+               <hr />
+               <Button
+                  variant='outline-dark'
+                  onClick={addInfo}>
+                  Добавить новое свойство
+               </Button>
+               {
+                  info.map(i =>
+                     <Row className='mt-3' key={i.number}>
+                        <Col md={4}>
+                           <Form.Control
+                              placeholder='Введите название характеристики'
+                           />
+                        </Col>
+                        <Col md={4}>
+                           <Form.Control
+                              placeholder='Введите описание'
+                           />
+                        </Col>
+                        <Col md={4}>
+                           <Button
+                              variant='outline-danger'
+                              onClick={removeInfo(i.number)}
+                           >
+                              Удалить
+                           </Button>
+                        </Col>
+
+                     </Row>
+                  )
+               }
+            </Form>
+         </Modal.Body>
+         <Modal.Footer>
+            <Button variant='outline-danger' onClick={onHide}>Закрыть</Button>
+            <Button variant='outline-success' onClick={onHide}>Добавить</Button>
+         </Modal.Footer>
+      </Modal>
+   )
+}
+
+export default CreateDevice
--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
+import CreateBrand from '../components/modals/CreateBrand'
+import CreateDevice from '../components/modals/CreateDevice'
+import CreateType from '../components/modals/CreateType'
 
 const Admin = () => {
+   const [brandVisible, setBrandVisible] = useState(false) // хуки состояния для модальных окон админки
+   const [typeVisible, setTypeVisible] = useState(false)
+   const [deviceVisible, setDeviceVisible] = useState(false)
+
    return (
-      <div>
-         <h1>admin</h1>
-      </div>
+      <Container className='d-flex flex-column'>
+         <Button variant='outline-dark'
+            className='mt-4 p-2'
+            onClick={() => setTypeVisible(true)}
+         >
+            Добавить тип
+         </Button>
+         <Button variant='outline-dark'
+            className='mt-4 p-2'
+            onClick={() => setBrandVisible(true)}
+         >
+            Добавить бренд
+         </Button>
+
+         <Button variant='outline-dark'
+            className='mt-4 p-2'
+            onClick={() => setDeviceVisible(true)}
+         >
+            Добавить устройство
+         </Button>
+         <CreateBrand show={brandVisible} onHide={() => setBrandVisible(false)} />
+         <CreateDevice show={deviceVisible} onHide={() => setDeviceVisible(false)} />
+         <CreateType show={typeVisible} onHide={() => setTypeVisible(false)} />
+      </Container>
    )
 }
 
